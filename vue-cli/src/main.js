40a2a88cf7dd7ddfa27903eb5034129c83c944e8
Gabriel Costa <gabrielncosta208@gmail.com>
2019-10-25 20:09:57 +0000

    Add files via upload

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -5,6 +5,7 @@ import VueRouter from 'vue-router';
 import { routes } from './routes';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 Vue.use(VueRouter);
 
 const router = new VueRouter({
